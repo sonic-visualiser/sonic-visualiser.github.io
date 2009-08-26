e17108a6dc336a2be85f4d90c613d0d1247d6519
Chris Cannam <cannam@all-day-breakfast.com>
2009-08-26 13:53:21 +0000

    ...

--- survey16-submit.php
+++ survey16-submit.php
@@ -1,14 +1,37 @@
 <?php
 
+$have = 0;
+
+$f = disk_free_space("/var/surveys");
+if ($f < 100000000) {
+   die("Survey storage error: please contact admin");
+}
+
+foreach (array_keys($_POST) as $key) {
+    if ($key != "misc") {
+        $value = $_POST[$key];
+        if ($value != "") {
+            $have = 1;
+        }
+    }
+}
+
+if (!$have) {
+
+header("Location: survey16.html");
+
+} else {
+
 header("Location: survey16-thanks.html");
 
 $trap = $_POST['misc'];
 
 $d = time();
+$r = rand();
 if ($trap == "") {
-   $filename = "/var/surveys/sv16/$d";
+   $filename = "/var/surveys/sv16/$d.$r";
 } else {
-   $filename = "/var/surveys/sv16/dubious/$d";
+   $filename = "/var/surveys/sv16/dubious/$d.$r";
 }
 
 $file = fopen($filename, "w");
@@ -25,5 +48,8 @@ if ($file) {
    fclose($file);
 }
 
+sleep(2);
+
+}
 ?>
 
