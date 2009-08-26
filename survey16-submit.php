6f7bfae2b34241d068fa7c78c99ce692167a341d
Chris Cannam <cannam@all-day-breakfast.com>
2009-08-26 13:24:20 +0000

    * survey bits

---
+++ survey16-submit.php
@@ -0,0 +1,29 @@
+<?php
+
+header("Location: survey16-thanks.html");
+
+$trap = $_POST['misc'];
+
+$d = time();
+if ($trap == "") {
+   $filename = "/var/survey/$d";
+} else {
+   $filename = "/var/survey/dubious/$d";
+}
+
+$file = fopen($filename, "w");
+
+if ($file) {
+   $i = 0;
+   fwrite($file, "ip: " . $_SERVER['REMOTE_ADDR'] . "\n");
+      foreach (array_keys($_POST) as $key) {
+          $value = $_POST[$key];
+	  if ($value == "") continue;
+	  fwrite($file, "$key: $value\n");
+	  $i = $i + 1;
+      }
+   fclose($file);
+}
+
+?>
+
